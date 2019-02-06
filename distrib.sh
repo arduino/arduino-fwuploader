b7e1cb538e835ce02e90a1c50349bcb1c4a07a7e
Martino Facchin <m.facchin@arduino.cc>
2019-02-06 12:06:00 +0100

    Only keep a copy of the firmwares

--- distrib.sh
+++ distrib.sh
@@ -3,7 +3,7 @@
 rm -rf distrib
 mkdir -p distrib/linux64
 mkdir -p distrib/linux32
-mkdir -p distrib/linuxar
+mkdir -p distrib/linuxarm
 mkdir -p distrib/linuxarm64
 mkdir -p distrib/osx
 mkdir -p distrib/windows
@@ -18,14 +18,9 @@ GOOS=windows GOARCH=386 GO386=387 go build -o distrib/windows/updater.exe github
 
 export CGO_ENABLED=1
 # need osxcross in path
-CC=o64-clang GOOS=darwin GOARCH=amd64 go build -o distrib/osx/updater.exe github.com/arduino-libraries/WiFi101-FirmwareUpdater/cli/main/
+CC=o64-clang GOOS=darwin GOARCH=amd64 go build -o distrib/osx/updater github.com/arduino-libraries/WiFi101-FirmwareUpdater/cli/main/
 
-cp -r firmwares distrib/linux64
-cp -r firmwares distrib/linux32
-cp -r firmwares distrib/linuxar
-cp -r firmwares distrib/linuxarm64
-cp -r firmwares distrib/osx
-cp -r firmwares distrib/windows
+cp -r firmwares distrib/
 
 # call the tool with something like
-# ./updater -flasher firmwares/NINA/FirmwareUpdater.mkrwifi1010.ino.bin -firmware firmwares/NINA/1.2.1/NINA_W102.bin -port /dev/ttyACM0  -address arduino.cc:443 -restore_binary /tmp/arduino_build_619137/WiFiSSLClient.ino.bin -programmer bossac/bossac
+# ./linux64/updater -flasher firmwares/NINA/FirmwareUpdater.mkrwifi1010.ino.bin -firmware firmwares/NINA/1.2.1/NINA_W102.bin -port /dev/ttyACM0  -address arduino.cc:443 -restore_binary /tmp/arduino_build_619137/WiFiSSLClient.ino.bin -programmer {runtime.tools.bossac}/bossac
