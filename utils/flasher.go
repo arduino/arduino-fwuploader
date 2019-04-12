a76d549a467efd3dc0cecfb59369583e4c294f81
Mattia Bertorello <m.bertorello@arduino.cc>
2019-04-12 15:12:52 +0200

    Improve compatibility with different system http://www.ni.com/product-documentation/54548/en/

--- utils/flasher.go
+++ utils/flasher.go
@@ -2,15 +2,40 @@ package utils
 
 import (
 	"github.com/facchinm/go-serial"
+	"log"
 )
 
-func OpenSerial(portName string) (serial.Port, error) {
-	mode := &serial.Mode{
-		// This bound rate works on osx 10.14
-		BaudRate: 115200,
-		Vtimeout: 255,
-		Vmin:     0,
-	}
+// http://www.ni.com/product-documentation/54548/en/
+var baudRates = []int{
+	//  Standard baud rates supported by some serial ports
+	921600,
+	460800,
+	256000,
+	230400,
+	153600,
+	128000,
+	// Standard baud rates supported by most serial ports
+	115200,
+	57600,
+	56000,
+	38400,
+}
 
-	return serial.Open(portName, mode)
+func OpenSerial(portName string) (serial.Port, error) {
+	var port serial.Port
+	var err error
+	for _, baudRate := range baudRates {
+		mode := &serial.Mode{
+			BaudRate: baudRate,
+			Vtimeout: 255,
+			Vmin:     0,
+		}
+		port, err := serial.Open(portName, mode)
+		if err == nil {
+			log.Printf("Open the serial port with baud rate %d", baudRate)
+			return port, nil
+		}
+	}
+	return port, err
+
 }
