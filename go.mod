775097c7490ea6f73ab4a094627dee301e790bc6
Cristian Maglie <c.maglie@bug.st>
2020-09-24 17:15:04 +0200

    Factored out reset/1200bps-touch subroutines and executils
    Reusing the implementation available in arduino-cli

--- go.mod
+++ go.mod
@@ -5,4 +5,8 @@ go 1.14
 // branch with support for serial timeouts
 replace go.bug.st/serial => github.com/cmaglie/go-serial v0.0.0-20200923162623-b214c147e37e
 
-require go.bug.st/serial v1.1.1
+require (
+	github.com/arduino/arduino-cli v0.0.0-20200924151007-69ac12c98b2b
+	github.com/pkg/errors v0.9.1
+	go.bug.st/serial v1.1.1
+)
--- programmers/bossac/bossac.go
+++ programmers/bossac/bossac.go
@@ -1,212 +1,74 @@
 package bossac
 
 import (
-	"bytes"
-	"errors"
 	"io/ioutil"
 	"log"
-	"os/exec"
+	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/arduino/FirmwareUpdater/utils/context"
-	"go.bug.st/serial"
+	"github.com/arduino/arduino-cli/arduino/serialutils"
+	"github.com/arduino/arduino-cli/executils"
+	"github.com/pkg/errors"
 )
 
 type Bossac struct {
 }
 
 func (b *Bossac) Flash(ctx *context.Context, filename string) error {
-	log.Println("Flashing " + filename)
-
-	port, err := reset(ctx.PortName, true)
+	log.Println("Entering board into bootloader mode")
+	port, err := serialutils.Reset(ctx.PortName, true)
 	if err != nil {
 		return err
 	}
+
+	log.Println("Flashing " + filename)
 	err = invokeBossac([]string{ctx.ProgrammerPath, "-e", "-R", "-p", port, "-w", filename})
 
-	ports, err := serial.GetPortsList()
-	ctx.PortName = waitPort(ports, port)
-
-	log.Println("Port is online " + ctx.PortName)
+	ctx.PortName, err = serialutils.WaitForNewSerialPortOrDefaultTo(port)
+	log.Println("Board is back online " + ctx.PortName)
 	time.Sleep(1 * time.Second)
 
 	return err
 }
 
 func (b *Bossac) DumpAndFlash(ctx *context.Context, filename string) (string, error) {
-	log.Println("Flashing " + filename)
 	dir, err := ioutil.TempDir("", "wifiFlasher_dump")
-	port, err := reset(ctx.PortName, true)
+	if err != nil {
+		return "", errors.WithMessage(err, "creating temp dir to store current sketch")
+	}
+
+	log.Println("Entering board into bootloader mode")
+	port, err := serialutils.Reset(ctx.PortName, true)
 	if err != nil {
 		return "", err
 	}
+
+	log.Println("Reading existing sketch from the baord, to restore it later")
 	err = invokeBossac([]string{ctx.ProgrammerPath, "-u", "-r", "-p", port, filepath.Join(dir, "dump.bin")})
+
 	log.Println("Original sketch saved at " + filepath.Join(dir, "dump.bin"))
 	if err != nil {
 		return "", err
 	}
+
+	log.Println("Flashing " + filename)
 	err = invokeBossac([]string{ctx.ProgrammerPath, "-e", "-R", "-p", port, "-w", filename})
 
-	ports, err := serial.GetPortsList()
-	ctx.PortName = waitPort(ports, port)
+	ctx.PortName, err = serialutils.WaitForNewSerialPortOrDefaultTo(port)
+	log.Println("Board is back online " + ctx.PortName)
+	time.Sleep(1 * time.Second)
 
 	return filepath.Join(dir, "dump.bin"), err
 }
 
 func invokeBossac(args []string) error {
-	cmd := exec.Command(args[0], args[1:]...)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	log.Println(out.String())
-	return err
-}
-
-func touchSerialPortAt1200bps(port string) error {
-	log.Println("Touching port " + port + " at 1200bps")
-
-	// Open port
-	p, err := serial.Open(port, &serial.Mode{BaudRate: 1200})
+	cmd, err := executils.NewProcess(args...)
 	if err != nil {
-		return errors.New("Open port " + port)
+		return err
 	}
-	defer p.Close()
-
-	// Set DTR
-	err = p.SetDTR(false)
-	log.Println("Set DTR off")
-	if err != nil {
-		return errors.New("Can't set DTR")
-	}
-
-	// Wait a bit to allow restart of the board
-	time.Sleep(200 * time.Millisecond)
-
-	return nil
-}
-
-// reset opens the port at 1200bps. It returns the new port name (which could change
-// sometimes) and an error (usually because the port listing failed)
-func reset(port string, wait bool) (string, error) {
-	log.Println("Restarting in bootloader mode")
-
-	// Get port list before reset
-	ports, err := serial.GetPortsList()
-	log.Println("Get port list before reset")
-	if err != nil {
-		return "", errors.New("Get port list before reset")
-	}
-
-	// Touch port at 1200bps
-	err = touchSerialPortAt1200bps(port)
-	if err != nil {
-		return "", errors.New("1200bps Touch")
-	}
-
-	// Wait for port to disappear and reappear
-	if wait {
-		port = waitReset(ports, port)
-	}
-
-	return port, nil
-}
-
-// waitReset is meant to be called just after a reset. It watches the ports connected
-// to the machine until a port disappears and reappears. The port name could be different
-// so it returns the name of the new port.
-func waitReset(beforeReset []string, originalPort string) string {
-	var port string
-	timeout := false
-
-	go func() {
-		time.Sleep(10 * time.Second)
-		timeout = true
-	}()
-
-	for {
-		ports, _ := serial.GetPortsList()
-		port = differ(ports, beforeReset)
-
-		if port != "" {
-			break
-		}
-		if timeout {
-			break
-		}
-		time.Sleep(time.Millisecond * 100)
-	}
-
-	// Wait for the port to reappear
-	log.Println("Wait for the port to reappear")
-	afterReset, _ := serial.GetPortsList()
-	for {
-		ports, _ := serial.GetPortsList()
-		port = differ(ports, afterReset)
-		if port != "" {
-			time.Sleep(time.Millisecond * 500)
-			break
-		}
-		if timeout {
-			break
-		}
-		time.Sleep(time.Millisecond * 100)
-	}
-
-	// try to upload on the existing port if the touch was ineffective
-	if port == "" {
-		port = originalPort
-	}
-
-	return port
-}
-
-func waitPort(beforeReset []string, originalPort string) string {
-	var port string
-	timeout := false
-
-	go func() {
-		time.Sleep(10 * time.Second)
-		timeout = true
-	}()
-
-	for {
-		ports, _ := serial.GetPortsList()
-		port = differ(ports, beforeReset)
-
-		if port != "" {
-			break
-		}
-		if timeout {
-			break
-		}
-		time.Sleep(time.Millisecond * 100)
-	}
-
-	// try to upload on the existing port if the touch was ineffective
-	if port == "" {
-		port = originalPort
-	}
-
-	return port
-}
-
-// differ returns the first item that differ between the two input slices
-func differ(slice1 []string, slice2 []string) string {
-	m := map[string]int{}
-
-	for _, s1Val := range slice1 {
-		m[s1Val] = 1
-	}
-	for _, s2Val := range slice2 {
-		m[s2Val] = m[s2Val] + 1
-	}
-
-	for mKey, mVal := range m {
-		if mVal == 1 {
-			return mKey
-		}
-	}
-
-	return ""
+	cmd.RedirectStdoutTo(os.Stdout)
+	cmd.RedirectStderrTo(os.Stderr)
+	return cmd.Run()
 }
