370de5c726e0c04f7dd64bef7bc09ac43ab2aded
Alberto Iannaccone <a.iannaccone@arduino.cc>
2020-09-25 15:22:50 +0200

    increase flasher read timeout to 30 seconds

--- utils/flasher.go
+++ utils/flasher.go
@@ -29,7 +29,7 @@ func OpenSerial(portName string) (serial.Port, error) {
 		}
 		log.Printf("Opened the serial port with baud rate %d", baudRate)
 
-		if err := port.SetReadTimeout(10 * time.Second); err != nil {
+		if err := port.SetReadTimeout(30 * time.Second); err != nil {
 			log.Fatalf("Could not set timeout on serial port: %s", err)
 			return nil, err
 		}
