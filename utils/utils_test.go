be1bd4fdd288e35802d873793ad7c699d8ab249b
Cristian Maglie <c.maglie@arduino.cc>
2020-10-13 10:59:31 +0100

    Slightly improved test verbosity

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -12,20 +12,22 @@ func TestGetCompatibleWith(t *testing.T) {
 	require.NoError(t, err)
 	require.NoError(t, root.ToAbs())
 	testrunner := func(board string) {
-		res := GetCompatibleWith(board, root.String())
-		require.NotNil(t, res)
-		hasLoader := false
-		for _, e := range res {
-			for _, i := range e {
-				if i.IsLoader {
-					require.False(t, hasLoader, "loader must be unique")
-					hasLoader = true
-					require.NotEmpty(t, i.Name)
-					require.NotEmpty(t, i.Path)
+		t.Run(board, func(t *testing.T) {
+			res := GetCompatibleWith(board, root.String())
+			require.NotNil(t, res)
+			hasLoader := false
+			for _, e := range res {
+				for _, i := range e {
+					if i.IsLoader {
+						require.False(t, hasLoader, "loader must be unique")
+						hasLoader = true
+						require.NotEmpty(t, i.Name)
+						require.NotEmpty(t, i.Path)
+					}
 				}
 			}
-		}
-		require.True(t, hasLoader, "loader must be present")
+			require.True(t, hasLoader, "loader must be present")
+		})
 	}
 
 	testrunner("mkrwifi1010")
