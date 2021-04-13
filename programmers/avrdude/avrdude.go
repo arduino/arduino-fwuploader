85e84e2268983cb87821f95481de5989da058b25
Cristian Maglie <c.maglie@arduino.cc>
2021-04-14 00:52:58 +0200

    Removed now unused DumpAndFlash methods

--- programmers/avrdude/avrdude.go
+++ programmers/avrdude/avrdude.go
@@ -2,16 +2,13 @@ package avrdude
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
 	"time"
 
 	"github.com/arduino/FirmwareUpdater/utils/context"
 	"github.com/arduino/arduino-cli/executils"
 	"github.com/arduino/go-paths-helper"
-	"github.com/pkg/errors"
 )
 
 type Avrdude struct {
@@ -44,45 +41,6 @@ func (b *Avrdude) Flash(filename string) error {
 	time.Sleep(3 * time.Second)
 
 	return err
-}
-
-func (b *Avrdude) DumpAndFlash(filename string) (string, error) {
-	dir, err := ioutil.TempDir("", "wifiFlasher_dump")
-	if err != nil {
-		return "", errors.WithMessage(err, "creating temp dir to store current sketch")
-	}
-
-	log.Println("Reading existing sketch from the baord, to restore it later")
-	err = b.invoke(
-		fmt.Sprintf("-C%s", b.configPath),
-		"-v",
-		"-patmega4809",
-		"-cxplainedmini_updi",
-		"-Pusb",
-		"-b115200",
-		"-D",
-		"-Uflash:r:"+filepath.Join(dir, "dump.bin")+":i")
-	if err != nil {
-		return "", err
-	}
-	log.Println("Original sketch saved at " + filepath.Join(dir, "dump.bin"))
-
-	log.Println("Flashing " + filename)
-	err = b.invoke(
-		fmt.Sprintf("-C%s", b.configPath),
-		"-v",
-		"-patmega4809",
-		"-cxplainedmini_updi",
-		"-Pusb",
-		"-b115200",
-		"-e",
-		"-D",
-		"-Uflash:w:"+filename+":i",
-		"-Ufuse8:w:0x00:m")
-
-	time.Sleep(3 * time.Second)
-
-	return filepath.Join(dir, "dump.bin"), err
 }
 
 func (b *Avrdude) invoke(args ...string) error {
--- programmers/bossac/bossac.go
+++ programmers/bossac/bossac.go
@@ -1,17 +1,14 @@
 package bossac
 
 import (
-	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
 	"time"
 
 	"github.com/arduino/FirmwareUpdater/utils/context"
 	"github.com/arduino/arduino-cli/arduino/serialutils"
 	"github.com/arduino/arduino-cli/executils"
 	"github.com/arduino/go-paths-helper"
-	"github.com/pkg/errors"
 )
 
 type Bossac struct {
@@ -41,36 +38,6 @@ func (b *Bossac) Flash(filename string) error {
 	time.Sleep(1 * time.Second)
 
 	return err
-}
-
-func (b *Bossac) DumpAndFlash(ctx *context.Context, filename string) (string, error) {
-	dir, err := ioutil.TempDir("", "wifiFlasher_dump")
-	if err != nil {
-		return "", errors.WithMessage(err, "creating temp dir to store current sketch")
-	}
-
-	log.Println("Entering board into bootloader mode")
-	port, err := serialutils.Reset(ctx.PortName, true)
-	if err != nil {
-		return "", err
-	}
-
-	log.Println("Reading existing sketch from the baord, to restore it later")
-	err = b.invoke("-u", "-r", "-p", port, filepath.Join(dir, "dump.bin"))
-
-	log.Println("Original sketch saved at " + filepath.Join(dir, "dump.bin"))
-	if err != nil {
-		return "", err
-	}
-
-	log.Println("Flashing " + filename)
-	err = b.invoke("-e", "-R", "-p", port, "-w", filename)
-
-	ctx.PortName, err = serialutils.WaitForNewSerialPortOrDefaultTo(port)
-	log.Println("Board is back online " + ctx.PortName)
-	time.Sleep(1 * time.Second)
-
-	return filepath.Join(dir, "dump.bin"), err
 }
 
 func (b *Bossac) invoke(args ...string) error {
