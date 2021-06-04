b380b2717c655b8a6574bfebc7343980e2d6d577
Umberto Baldi <u.baldi@arduino.cc>
2021-06-04 11:46:17 +0200

    add first implementation of module_firmware_index struct to parse json

---
+++ module_firmware_index/module_firmware_index.go
@@ -0,0 +1,108 @@
+/*
+  FirmwareUploader
+  Copyright (c) 2021 Arduino LLC.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+package index
+
+import (
+	"encoding/json"
+
+	"github.com/arduino/arduino-cli/arduino/security"
+	"github.com/arduino/go-paths-helper"
+	"github.com/sirupsen/logrus"
+)
+
+// Index represents Boards struct as seen from module_firmware_index.json file.
+type Index struct {
+	Boards    []indexBoard // correct?
+	IsTrusted bool
+}
+
+// indexPackage represents a single entry from module_firmware_index.json file.
+type indexBoard struct {
+	Fqbn            string             `json:"fqbn"` // TODO what's "required"?
+	Firmwares       []indexFirmware    `json:"firmware"`
+	LoaderSketch    *indexLoaderSketch `json:"loader_sketch"` // single object?
+	Module          string             `json:"module"`
+	Name            string             `json:"name"`
+	Uploader        string             `json:"uploader"`
+	UploadTouch     string             `json:"upload.use_1200bps_touch"`    // TODO remove "" in json otherwise is a string and not a bool
+	UploadWait      string             `json:"upload.wait_for_upload_port"` // TODO see above
+	UploaderCommand string             `json:"uploader.command"`
+}
+
+// indexFirmware represents a single Firmware version from module_firmware_index.json file.
+type indexFirmware struct {
+	Version  string      `json:"version"` // use `*semver.Version` instead but SARA version is giving problems
+	URL      string      `json:"url"`
+	Checksum string      `json:"checksum"`
+	Size     json.Number `json:"size"`
+}
+
+// indexLoaderSketch represents the sketch used to upload the new firmware on a board.
+type indexLoaderSketch struct {
+	URL      string      `json:"url"`
+	Checksum string      `json:"checksum"`
+	Size     json.Number `json:"size"`
+}
+
+// LoadIndex reads a module_firmware_index.json from a file and returns the corresponding Index structure.
+func LoadIndex(jsonIndexFile *paths.Path) (*Index, error) {
+	buff, err := jsonIndexFile.ReadFile()
+	if err != nil {
+		return nil, err
+	}
+	var index Index
+	err = json.Unmarshal(buff, &index)
+	if err != nil {
+		return nil, err
+	}
+
+	jsonSignatureFile := jsonIndexFile.Parent().Join(jsonIndexFile.Base() + ".sig")
+	trusted, _, err := security.VerifyArduinoDetachedSignature(jsonIndexFile, jsonSignatureFile)
+	if err != nil {
+		logrus.
+			WithField("index", jsonIndexFile).
+			WithField("signatureFile", jsonSignatureFile).
+			WithError(err).Infof("Checking signature")
+	} else {
+		logrus.
+			WithField("index", jsonIndexFile).
+			WithField("signatureFile", jsonSignatureFile).
+			WithField("trusted", trusted).Infof("Checking signature")
+		index.IsTrusted = trusted
+	}
+	return &index, nil
+}
+
+// LoadIndexNoSign reads a module_firmware_index.json from a file and returns the corresponding Index structure.
+func LoadIndexNoSign(jsonIndexFile *paths.Path) (*[]indexBoard, error) {
+	buff, err := jsonIndexFile.ReadFile()
+	if err != nil {
+		return nil, err
+	}
+	var index []indexBoard
+	err = json.Unmarshal(buff, &index)
+	if err != nil {
+		return nil, err
+	}
+
+	// index.IsTrusted = true
+
+	return &index, nil
+}
---
+++ module_firmware_index/module_firmware_index_test.go
@@ -0,0 +1,53 @@
+/*
+  FirmwareUploader
+  Copyright (c) 2021 Arduino LLC.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+package index
+
+import (
+	"os"
+	"path"
+	"strings"
+	"testing"
+
+	"github.com/arduino/FirmwareUploader/indexes"
+	"github.com/arduino/arduino-cli/arduino/utils"
+	"github.com/arduino/go-paths-helper"
+	"github.com/stretchr/testify/require"
+)
+
+var DefaultIndexGZURL = []string{
+	"http://downloads-dev.arduino.cc/arduino-fwuploader/boards/module_firmware_index.json.gz",
+}
+
+func TestIndexParsing(t *testing.T) {
+	// semver.WarnInvalidVersionWhenParsingRelaxed = true
+	for _, u := range DefaultIndexGZURL {
+		t.Logf("testing with index: %s", u)
+		err := indexes.DownloadIndex(u) // prepare for the test (indexes must be downloaded)
+		require.NoError(t, err)
+		indexFolder := paths.TempDir().Join("fwuploader")
+		URL, err := utils.URLParse(u)
+		require.NoError(t, err)
+		indexPath := indexFolder.Join(path.Base(strings.ReplaceAll(URL.Path, ".gz", "")))
+		require.FileExists(t, indexPath.String())
+		_, e := LoadIndexNoSign(indexPath)
+		require.NoError(t, e)
+		defer os.RemoveAll(indexFolder.String())
+	}
+}
