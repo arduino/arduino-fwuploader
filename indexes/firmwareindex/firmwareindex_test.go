13653fc078c99cf752b80f8991d3b289ac06d4d2
per1234 <accounts@perglass.com>
2021-11-21 21:35:53 -0800

    Relicense project as AGPL-3.0-or-later
    This changes the licensing of Arduino Firmware Uploader from GNU Lesser General Public License v2.1 to GNU Affero General
    Public License v3.0.
    
    The "or any later version" clause is provided (SPDX license identifier AGPL-3.0-or-later).
    
    I did my best to assess the intellectual property of this codebase and make sure all contributors and copyright owners
    granted us permission to relicense their code. For any inquiry, report or claim please contact us at license@arduino.cc.

--- indexes/firmwareindex/firmwareindex_test.go
+++ indexes/firmwareindex/firmwareindex_test.go
@@ -2,19 +2,18 @@
 	arduino-fwuploader
 	Copyright (c) 2021 Arduino LLC.  All right reserved.
 
-	This library is free software; you can redistribute it and/or
-	modify it under the terms of the GNU Lesser General Public
-	License as published by the Free Software Foundation; either
-	version 2.1 of the License, or (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published
+	by the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-	This library is distributed in the hope that it will be useful,
+	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-	Lesser General Public License for more details.
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
 
-	You should have received a copy of the GNU Lesser General Public
-	License along with this library; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 package firmwareindex
